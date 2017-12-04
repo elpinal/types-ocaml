7264900ea62524c019b133c786a72253944c81c4
elpinal <elpinal@users.noreply.github.com>
2017-12-04 20:35:14 +0900

    Implement fibonacci series

--- types.ml
+++ types.ml
@@ -10,3 +10,11 @@ let rec upfrom n = Stream (fun () -> (n, upfrom (n + 1)))
 
 let () = upfrom 10 |> tl |> tl |> hd |> print_int;
          print_newline ()
+
+let rec fib (m, n) = Stream (fun () -> (m, fib (n, m + n)))
+
+let () = let s = fib (1, 1) in
+         let s = s |> tl |> tl |> tl |> tl |> tl in
+         let n = hd s in
+         print_int n;
+         print_newline ()
