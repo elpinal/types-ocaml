95a32b96962973224b23765f5d5c3fded01aeb57
elpinal <elpinal@users.noreply.github.com>
2017-12-05 16:56:04 +0900

    Add ty type

--- kinds.ml
+++ kinds.ml
@@ -11,3 +11,18 @@ let () = let k = Star in
          str_kind (Arrow (k, (Arrow (k, k)))) |> print_endline;
          str_kind (Arrow ((Arrow (k, k)), k)) |> print_endline;
          str_kind (Arrow (k, k)) |> print_endline
+
+type ty = Var of string
+        | Con of string
+        | App of ty * ty
+
+let rec str_ty =
+  function Var i -> i
+         | Con i -> i
+         | App (t1, (App (t2, t3) as a)) -> str_ty t1 ^ " (" ^ str_ty a ^ ")"
+         | App (t1, t2) -> str_ty t1 ^ " " ^ str_ty t2
+
+let () = let tv = Var "a" in
+         let tc = Con "A" in
+         str_ty tv |> print_endline;
+         str_ty (App (tc, (App (tc, tv)))) |> print_endline;
